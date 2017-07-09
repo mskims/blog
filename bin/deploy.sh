fd361efa3910b383c0748306e03a377d43dfddee
mskims <mskim@quicket.co.kr>
2017-07-09 23:12:22 +0900

    Change deploy script (especially git scripts)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Run this script to deploy the app to Github Pages.
 
@@ -13,15 +13,19 @@ bundle exec jekyll build
 
 # Delete and move files.
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv _site/* .
-rm -R _site/
+
+# go to the out directory and create a *new* Git repo
+cd _site
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "mskims"
+git config user.email "its@mskim.me"
 
 # Push to gh-pages.
-git add -fA
+git add .
 git commit --allow-empty -m "Deploy to gh-pages [ci skip]"
 
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/mskims/blog.git" master:gh-pages
 
 echo "Deployed Successfully!"
-
-exit 0
